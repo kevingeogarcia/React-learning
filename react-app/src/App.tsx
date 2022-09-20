38e002091ff08ee663ac5c3a0d9f6fa0fdf7f833
kevingeogarcia <kevingeogarcia@gmail.com>
2022-09-19 20:44:22 -0600

    add project to structure based routes by react-router-dom

---
+++ react-app/src/App.tsx
@@ -0,0 +1,11 @@
+import { Navigation } from "./routes/Navigation";
+
+function App() {
+  return (
+    <>
+    <Navigation></Navigation>
+    </>
+  );
+}
+
+export default App;
---
+++ react-app/src/routes/Navigation.tsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route, NavLink, Navigate } from 'react-router-dom';
+import logo from '../logo.svg';
+export const Navigation = () => {
+  return (
+    <BrowserRouter>
+      <div className="main-layout">
+        <nav><img src={logo} alt="React logo" />
+          <ul>
+          <li>
+              <NavLink className={({ isActive }) => isActive ? 'nav-active' : ''} to='/home'>Home</NavLink>
+            </li>
+            <li>
+              <NavLink className={({ isActive }) => isActive ? 'nav-active' : ''} to='/about'> About</NavLink>
+            </li>
+            <li>
+              <NavLink className={({ isActive }) => isActive ? 'nav-active' : ''} to='/users'> Users</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+
+        <Routes>
+          <Route path="about" element={<h1>About Page</h1>} />
+          <Route path="users" element={<h1>Users Page</h1>} />
+          <Route path="/home" element={<h1>Home Page</h1>} />
+          <Route path="/*" element={<Navigate to='/home' replace />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
